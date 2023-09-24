9d96b6492bd1e16f9da3405f39f0b1dabfae7308
Oseghale913 <abhuluimenose@gmail.com>
2023-09-24 10:15:14 -0700

    updated correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,11 +4,11 @@
 /**
  * main - Entry point
  *
- * Return: Always 1 (Succcess)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
 	write(2,
-	"and that piece of art is useful\" - Dora Korper, 2015-10-19\n" 59);
+	"and that piece of art is useful\" - Dora Korper, 2015-10-19\n", 59);
 	return (1);
 }
