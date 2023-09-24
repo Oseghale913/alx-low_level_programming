bdacf9bdf448bdb1d06ef38a8daf05bdb46d9533
Oseghale913 <abhuluimenose@gmail.com>
2023-09-24 10:28:04 -0700

    redo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Enrty point
  *
@@ -6,7 +7,7 @@
  */
 int main(void)
 {
-	puts("\"Programing is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	
 	return (0);
 }
