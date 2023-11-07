7796e4e341a537b6cd3b62796ca7537a6cbbc810
Matthew Oseghale <abhuluimenose@gmail.com>
2023-11-07 05:20:40 +0100

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,6 +13,7 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
 	char *s;
+
 	if (size == 0)
 		return (NULL);
 
@@ -24,8 +25,6 @@ char *create_array(unsigned int size, char c)
 	{
 		s[i] = c;
 	}
-	
+
 	return (s);
 }
-
-
